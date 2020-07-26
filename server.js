1369469c0914e442c756a6cbf94d933733fed77a
Kamil Bogucki <boguckikamil@hotmail.com>
2020-07-26 10:28:58 +0200

    add file input to the form

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+var multer  = require('multer')
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
+
+const upload = multer();
 
 app.use((req, res, next) => {
     res.show = (name) => {
@@ -19,6 +22,9 @@ app.use('/user', (req, res, next) => {
   });
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+
 
 app.get('/hello/:name', (req, res) => {
     res.render('Hello', { name: req.params.name });
@@ -37,16 +43,30 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact', { layout: 'dark'});
+    res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-    res.render('info');
+    res.render('info', { layout: 'dark'});
 });
 
 app.get('/history', (req, res) => {
     res.render('history');
 });
+
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+
+    const { author, sender, title, message } = req.body;
+  
+    if(author && sender && title && message && req.file.originalname) {
+        res.render('contact', { isSent: true, name: req.file.originalname});
+        console.log(req.file)
+      }
+      else {
+        res.render('contact', { isError: true });
+      }
+  
+  });
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname,'/public/error.jpg'));
